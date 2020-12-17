7b9900eec5e9528e12c25a8b449df4f1b427f2ad
9426224 <9426224@live.com>
2020-12-17 13:11:38 +0800

    加入空洞填充滤波算法，使用多次均值滤波对深度图像的空洞进行填充补偿

--- Stereo/DepthImg.cpp
+++ Stereo/DepthImg.cpp
@@ -85,7 +85,97 @@ void DepthImg::Play(unsigned char* buf)
 
 void DepthImg::DepthImageProcess(Mat img)
 {
-	imshow("Depth",img);
+    img = insertDepth32f(img);
 
+	imshow("Depth",img);
 }
 
+Mat insertDepth32f(cv::Mat& depth)
+{
+    const int width = depth.cols;
+    const int height = depth.rows;
+    float* data = (float*)depth.data;
+    cv::Mat integralMap = cv::Mat::zeros(height, width, CV_64F);
+    cv::Mat ptsMap = cv::Mat::zeros(height, width, CV_32S);
+    double* integral = (double*)integralMap.data;
+    int* ptsIntegral = (int*)ptsMap.data;
+    memset(integral, 0, sizeof(double) * width * height);
+    memset(ptsIntegral, 0, sizeof(int) * width * height);
+    for (int i = 0; i < height; ++i)
+    {
+        int id1 = i * width;
+        for (int j = 0; j < width; ++j)
+        {
+            int id2 = id1 + j;
+            if (data[id2] > 1e-3)
+            {
+                integral[id2] = data[id2];
+                ptsIntegral[id2] = 1;
+            }
+        }
+    }
+    // 积分区间
+    for (int i = 0; i < height; ++i)
+    {
+        int id1 = i * width;
+        for (int j = 1; j < width; ++j)
+        {
+            int id2 = id1 + j;
+            integral[id2] += integral[id2 - 1];
+            ptsIntegral[id2] += ptsIntegral[id2 - 1];
+        }
+    }
+    for (int i = 1; i < height; ++i)
+    {
+        int id1 = i * width;
+        for (int j = 0; j < width; ++j)
+        {
+            int id2 = id1 + j;
+            integral[id2] += integral[id2 - width];
+            ptsIntegral[id2] += ptsIntegral[id2 - width];
+        }
+    }
+    int wnd;
+    double dWnd = 2;
+    while (dWnd > 1)
+    {
+        wnd = int(dWnd);
+        dWnd /= 2;
+        for (int i = 0; i < height; ++i)
+        {
+            int id1 = i * width;
+            for (int j = 0; j < width; ++j)
+            {
+                int id2 = id1 + j;
+                int left = j - wnd - 1;
+                int right = j + wnd;
+                int top = i - wnd - 1;
+                int bot = i + wnd;
+                left = max(0, left);
+                right = min(right, width - 1);
+                top = max(0, top);
+                bot = min(bot, height - 1);
+                int dx = right - left;
+                int dy = (bot - top) * width;
+                int idLeftTop = top * width + left;
+                int idRightTop = idLeftTop + dx;
+                int idLeftBot = idLeftTop + dy;
+                int idRightBot = idLeftBot + dx;
+                int ptsCnt = ptsIntegral[idRightBot] + ptsIntegral[idLeftTop] - (ptsIntegral[idLeftBot] + ptsIntegral[idRightTop]);
+                double sumGray = integral[idRightBot] + integral[idLeftTop] - (integral[idLeftBot] + integral[idRightTop]);
+                if (ptsCnt <= 0)
+                {
+                    continue;
+                }
+                data[id2] = float(sumGray / ptsCnt);
+            }
+        }
+        int s = wnd / 2 * 2 + 1;
+        if (s > 201)
+        {
+            s = 201;
+        }
+        cv::GaussianBlur(depth, depth, cv::Size(s, s), s, s);
+    }
+    return depth;
+}
