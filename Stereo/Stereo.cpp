0da97b0a2835e896219035cf5ffa35cfe49166d6
9426224 <9426224@live.com>
2021-01-27 18:01:53 +0800

    -与MYNT-EYE SDK进行对比测试查询函数功能

--- Stereo/Stereo.cpp
+++ Stereo/Stereo.cpp
@@ -6,41 +6,43 @@
 using namespace std;
 using namespace cv;
 
-int main(void)
+int main()
 {
 	//Parameters
-	void *hEtronDI = nullptr, *pHandleEtronDI = nullptr;
-	void *pHwndNotice = nullptr;
-	DEVSELINFO pDevSelInfo;
-	vector<DEVINFORMATION> devInfo;
-	ETRONDI_STREAM_INFO pStreamColorInfo[64], pStreamDepthInfo[64];
-	CONTROL_MODE cm = IMAGE_SN_NONSYNC;
-	DEPTH_TRANSFER_CTRL dtc = DEPTH_IMG_NON_TRANSFER;
+	void *hEtronDI = nullptr; //初始化设备指针
+	void *pHandleEtronDI = nullptr; //实际设备指针
+	void *pHwndNotice = nullptr; //
+	DEVSELINFO pDevSelInfo; //设备选择信息
+	vector<DEVINFORMATION> devInfo; //设备信息存储
+	ETRONDI_STREAM_INFO pStreamColorInfo[64], pStreamDepthInfo[64];//深度流与彩色流
 	int pFPS = 30;
 	unsigned long int pDepthImageSize = sizeof(unsigned char) * 1280 * 720 *3;
-	unsigned char *pDepthBuf = (unsigned char*)malloc(pDepthImageSize);
+	unsigned char *pDepthBuf = (unsigned char*)malloc(pDepthImageSize); //深度图像存储地址
 
-	Mat depthImg;
+	unsigned long int pColorImageSize = sizeof(unsigned char) * 1280 * 720 *3;
+	
+	unsigned char *pColorBuf = (unsigned char*)malloc(pColorImageSize); //深度图像存储地址
+
+	if(pDepthBuf == NULL)
+	{
+		cout<<"Create Swap Failed!"<<endl;
+		return -1;
+	}
+
+	Mat depthImg, colorImg;
 
 	//Function
 
 	//Device PreInit
 	if (EtronDI_Init(&hEtronDI, false) < 0)
 	{
-<<<<<<< HEAD
 		EtronDI_Release(&hEtronDI);
 		cout<<"Can't PreInit Device!"<<endl;
-=======
-		if (colorImg != nullptr)
-		{
-			colorImg->Play(imgBuf);
-		}
->>>>>>> 490de92a9effeff34657ba282046517875ce2c34
+		return -1;
 	}
 
 	if (hEtronDI != nullptr)
 	{
-<<<<<<< HEAD
 		int deviceCount = EtronDI_GetDeviceNumber(hEtronDI);	
 	    if(deviceCount > 0)
 		{
@@ -54,18 +56,13 @@ int main(void)
 				devInfo.push_back(devInformation);
 			}
 			EtronDI_Release(&hEtronDI);
-=======
-		if (depthImg != nullptr)
-		{
-			depthImg->Play(imgBuf);
->>>>>>> 490de92a9effeff34657ba282046517875ce2c34
 		}
 	}
 	
 	//Device Init
 	for(size_t i = 0; i < devInfo.size(); i++)
 	{
-		if(strcmp(devInfo[i].strDevName, "/dev/video0") == 0)
+		if(strcmp(devInfo[i].strDevName, "/dev/video1") == 0)
 		{
 			pDevSelInfo.index = i;
 		}
@@ -77,16 +74,39 @@ int main(void)
 
 	EtronDI_SetDepthDataType(pHandleEtronDI, &pDevSelInfo, ETronDI_DEPTH_DATA_11_BITS);
 	
-	if(EtronDI_OpenDevice(pHandleEtronDI, &pDevSelInfo, 1280, 720, true, 1280, 720, dtc, false, pHwndNotice, &pFPS, cm) == ETronDI_OK)
+	if(EtronDI_OpenDevice2(
+			pHandleEtronDI, 
+			&pDevSelInfo, 
+			1280, 
+			720, 
+			true, 
+			1280, 
+			720, 
+			DEPTH_IMG_NON_TRANSFER, 
+			false, 
+			NULL, 
+			&pFPS ) == ETronDI_OK)
 	{
 		cout<<"Open Device Success!"<<endl;
+	}
+	else
+	{
+		cout<<"Open Device Failed!"<<endl;
+		return -1;
 	}
 
 	while(true)
 	{
 		EtronDI_GetDepthImage(pHandleEtronDI, &pDevSelInfo, pDepthBuf, &pDepthImageSize, 0, 0);
 		depthImg = Mat(Size(1280, 720), CV_8UC1, pDepthBuf);
-		imshow("depth", depthImg);
+		//imshow("depth", depthImg);
+
+		EtronDI_GetColorImage(pHandleEtronDI, &pDevSelInfo, pColorBuf, &pColorImageSize, 0,0);
+
+		colorImg = Mat(Size(1280,720), CV_8UC3, pColorBuf);
+
+		//imshow("color", colorImg);
+
 		usleep(100000);
 		
 		int key = waitKey(1);
