d13fa1ae4fb3ab40e7adabc6246054e624bd1ef2
9426224 <9426224@live.com>
2021-01-28 17:55:28 +0800

    -删除无用的win平台下源代码

--- Windows-CPP/stdafx.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-typedef unsigned int UINT;
-typedef unsigned long       DWORD;
-typedef int                 BOOL;
-typedef unsigned char       BYTE;
-typedef unsigned short      WORD;
-typedef float               FLOAT;
-#define FALSE false
-#define NULL 0
